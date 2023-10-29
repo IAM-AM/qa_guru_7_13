bcb8792e00b5ba8536749dbe51bd5f0ecff81184
IAM-AM <aliaksandr.manko1@gmail.com>
2023-10-29 18:17:14 +0400

    Updated structure

--- demo_qa/tests/test_registration_form.py
+++ demo_qa/tests/test_registration_form.py
@@ -1,52 +1,73 @@
 import allure
-from demo_qa.data import users
+from allure_commons._allure import step
 from allure_commons.types import Severity
-import pytest
-from demo_qa.data.users import User
-from demo_qa.model.registration_form import RegistrationForm
+from demo_qa.pages.registration_page import RegistrationPage
 
 
-def test_user():
-    test_user = User(
-        first_name='Jack',
-        last_name='Sparrow',
-        email='Jack@pirate.com',
-        gender='Male',
-        phone_number='2960411232',
-        date_of_birth_day=20,
-        date_of_birth_month=6,
-        date_of_birth_year=1900,
-        subject=['Economics'],
-        hobby=['Reading'],
-        picture='test_pict.png',
-        current_address='1301 K Street NW Washington, DC 20071.',
-        state='NCR',
-        city='Noida'
-    )
-    return test_user
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Aliaksandr Manko")
+@allure.feature("Registration Form")
+@allure.story("User is successfully registered")
+@allure.link("https://demoqa.com/", name="Testing")
+def test_successful_student_registration_form():
+    with step("Open registration page"):
+        registration_page = RegistrationPage()
+        registration_page.open()
 
+    with step("Fill in the first name"):
+        registration_page.fill_first_name("Jack")
 
-@allure.title('Successful user registration form')
-def test_successful_registration(setup_browser, test_user):
-    registration_page = RegistrationForm()
-    registration_page.open_site()
+    with step("Fill in the last name"):
+        registration_page.fill_last_name("Sparrow")
 
-    registration_page.first_name(test_user.first_name)
-    registration_page.last_name(test_user.last_name)
-    registration_page.email(test_user.email)
-    registration_page.gender(test_user.gender)
-    registration_page.phone_number(test_user.phone_number)
-    registration_page.birth_date(test_user.date_of_birth_day,
-                                 test_user.date_of_birth_month,
-                                 test_user.date_of_birth_year
-                                 )
+    with step("Fill in the email"):
+        registration_page.fill_user_email("Jack@pirate.com")
 
-    registration_page.subject(test_user.subject)
-    registration_page.hobby(test_user.hobby)
-    registration_page.upload_picture(test_user.picture)
-    registration_page.current_address(test_user.current_address)
-    registration_page.state(test_user.state)
-    registration_page.city(test_user.city)
+    with step("Fill in the gender"):
+        registration_page.fill_gender("Male")
 
-    registration_page.press_submit()
-    registration_page.should_have_registered(test_user)
+    with step("Fill in the phone number"):
+        registration_page.fill_phone_number("6049524551")
+
+    with step("Fill in the birthday date"):
+        registration_page.fill_date_of_birth("July", "1900", "23")
+
+    with step("Fill in the subjects"):
+        registration_page.fill_subjects("Economics")
+
+    with step("Fill in the hobbies"):
+        registration_page.fill_hobbies("Reading")
+
+    with step("Fill in the picture"):
+        registration_page.upload_picture("test_image.png")
+
+    with step("Fill in the current address"):
+        registration_page.fill_current_address(
+            "1301 K Street NW Washington, DC 20071."
+        )
+    with step("Fill in the state"):
+        registration_page.fill_state("NCR")
+
+    with step("Fill in the city"):
+        registration_page.fill_city("Noida")
+
+    with step("Submit the form"):
+        registration_page.submit_form()
+
+    with step("Verify the confirmation modal form pops up"):
+        registration_page.modal_form_pops_up()
+
+    with step("Verify the student is registered with the correct data"):
+        registration_page.should_be_registered(
+            "Jack Sparrow",
+            "Jack@pirate.com",
+            "Male",
+            "6049524551",
+            "23 July,1900",
+            "Economics",
+            "Reading",
+            "test_image.png",
+            "1301 K Street NW Washington, DC 20071.",
+            "NCR Noida",
+        )
